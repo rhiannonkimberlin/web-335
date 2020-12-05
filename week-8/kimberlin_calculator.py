fe1cf40442161ab05d20ce875ce66cb97af82139
rhiannonkimberlin <r.kim@live.com>
2020-12-05 14:27:24 -0700

    Adding Python Week 8

---
+++ week-8/kimberlin_calculator.py
@@ -0,0 +1,30 @@
+"""
+============================================
+; Title:  kimberlin_calculator.py
+; Author: Professor Krasso
+; Date:  December 5 2020
+; Modified by: Rhiannon Kimberlin
+; Description: Calculator 
+===========================================
+"""
+
+first_name = 'Rhiannon'
+last_name = 'Kimberlin'
+print(first_name + ' ' + last_name)
+
+#Step 2 Add Function
+def add(number1, number2):
+    return number1 + number2
+
+#Step 3 Subtract Function
+def subtract(number1, number2):
+    return number1 - number2
+
+#Step 4 Divide Function
+def Divide(number1, number2):
+    return number1 / number2
+
+#Step 5 Call specified functions
+print(add(1, 2))
+print(subtract(4, 1))
+print(Divide(8, 2))
